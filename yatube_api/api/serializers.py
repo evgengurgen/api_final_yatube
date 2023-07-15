7dbba8dafc0b9e723f6e40d58dbd87698bdff25c
Евгений Волобуев <afondurin@yandex.ru>
2023-07-15 11:21:47 +0300

    API_yatube final hw untested version

--- yatube_api/api/serializers.py
+++ yatube_api/api/serializers.py
@@ -1,23 +1,59 @@
 from rest_framework import serializers
-from rest_framework.relations import SlugRelatedField
+
+from posts.models import Comment, Follow, Group, Post, User
 
 
-from posts.models import Comment, Post
+class UserSerializer(serializers.ModelSerializer):
+    posts = serializers.SlugRelatedField(many=True, read_only=True,
+                                         slug_field='text')
+
+    class Meta:
+        model = User
+        fields = '__all__'
+        ref_name = 'ReadOnlyUsers'
+
+
+class FollowSerializer(serializers.ModelSerializer):
+    user = serializers.SlugRelatedField(read_only=True,
+                                        slug_field='username')
+    following = serializers.SlugRelatedField(queryset=User.objects.all(),
+                                             slug_field='username')
+
+    class Meta:
+        model = Follow
+        fields = ('user', 'following')
+
+    def validate(self, data):
+        if data['following'] == self.context['request'].user:
+            raise serializers.ValidationError('Нельзя подписаться'
+                                              ' на самого себя!')
+        elif Follow.objects.filter(following=data['following']).exists():
+            raise serializers.ValidationError('Вы уже подписаны'
+                                              ' на этого автора!')
+        return data
 
 
 class PostSerializer(serializers.ModelSerializer):
-    author = SlugRelatedField(slug_field='username', read_only=True)
+    author = serializers.StringRelatedField(read_only=True)
+    pub_date = serializers.DateTimeField(read_only=True)
 
     class Meta:
-        fields = '__all__'
         model = Post
+        # Указал явно, чтобы порядок полей соответствовал документации.
+        fields = ('id', 'author', 'text', 'pub_date', 'image', 'group')
+
+
+class GroupSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Group
+        fields = '__all__'
 
 
 class CommentSerializer(serializers.ModelSerializer):
-    author = serializers.SlugRelatedField(
-        read_only=True, slug_field='username'
-    )
+    author = serializers.StringRelatedField(read_only=True)
 
     class Meta:
-        fields = '__all__'
         model = Comment
+        fields = '__all__'
+        read_only_fields = ('created', 'post')
--- yatube_api/api/views.py
+++ yatube_api/api/views.py
@@ -1,1 +1,99 @@
-# TODO:  Напишите свой вариант
+import datetime as dt
+
+from django.shortcuts import get_object_or_404
+from rest_framework import filters, mixins, viewsets
+from rest_framework.exceptions import PermissionDenied
+from rest_framework.pagination import LimitOffsetPagination
+
+from posts.models import Follow, Group, Post
+from .permissions import ReadOnly
+from .serializers import (CommentSerializer, FollowSerializer, GroupSerializer,
+                          PostSerializer)
+
+
+class CreateListViewSet(mixins.CreateModelMixin,
+                        mixins.ListModelMixin,
+                        viewsets.GenericViewSet):
+    pass
+
+
+class PostViewSet(viewsets.ModelViewSet):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+    pagination_class = LimitOffsetPagination
+
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user, pub_date=dt.datetime.now())
+
+    def perform_update(self, serializer):
+        if serializer.instance.author != self.request.user:
+            raise PermissionDenied('У вас недостаточно прав'
+                                   ' для выполнения данного действия.')
+        super().perform_update(serializer)
+
+    def perform_destroy(self, instance):
+        if instance.author != self.request.user:
+            raise PermissionDenied('У вас недостаточно прав'
+                                   ' для выполнения данного действия.')
+        instance.delete()
+
+    def get_permissions(self):
+        if self.action == 'retrieve' or self.action == 'list':
+            return (ReadOnly(),)
+        return super().get_permissions()
+
+
+class GroupViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = Group.objects.all()
+    serializer_class = GroupSerializer
+
+    def get_permissions(self):
+        if self.action == 'retrieve' or self.action == 'list':
+            return (ReadOnly(),)
+        return super().get_permissions()
+
+
+class FollowViewSet(CreateListViewSet):
+    serializer_class = FollowSerializer
+    filter_backends = (filters.SearchFilter,)
+    search_fields = ('following__username',)
+
+    def get_queryset(self):
+        return Follow.objects.filter(user=self.request.user)
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
+
+
+class CommentViewSet(viewsets.ModelViewSet):
+    serializer_class = CommentSerializer
+
+    def get_queryset(self):
+        post_id = self.kwargs.get('post_id')
+        post = get_object_or_404(Post, id=post_id)
+        return post.comments
+
+    def perform_create(self, serializer):
+        get_object_or_404(Post, id=self.kwargs.get('post_id'))
+        serializer.save(author=self.request.user,
+                        created=dt.datetime.now(),
+                        post_id=self.kwargs.get('post_id'))
+
+    def perform_update(self, serializer):
+        get_object_or_404(Post, id=self.kwargs.get('post_id'))
+        if serializer.instance.author != self.request.user:
+            raise PermissionDenied('У вас недостаточно прав'
+                                   ' для выполнения данного действия.')
+        super().perform_update(serializer)
+
+    def perform_destroy(self, instance):
+        get_object_or_404(Post, id=self.kwargs.get('post_id'))
+        if instance.author != self.request.user:
+            raise PermissionDenied('У вас недостаточно прав'
+                                   ' для выполнения данного действия.')
+        instance.delete()
+
+    def get_permissions(self):
+        if self.action == 'retrieve' or self.action == 'list':
+            return (ReadOnly(),)
+        return super().get_permissions()
